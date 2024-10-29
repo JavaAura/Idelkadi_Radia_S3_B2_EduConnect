155415700133aeba55a1c764fa07994a0b20d853
radia idelkadi <idelkadiradia@gmail.com>
2024-10-29 10:04:02 +0100

    ED-35: Create properties files for development and production environments

--- src/main/java/com/spring/eduConnect/EduConnectApplication.java
+++ src/main/java/com/spring/eduConnect/EduConnectApplication.java
@@ -2,12 +2,25 @@ package com.spring.eduConnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class EduConnectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EduConnectApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+//        SpringApplication.run(EduConnectApplication.class, args);
+
+        ApplicationContext context = SpringApplication.run(EduConnectApplication.class, args);
+        Environment env = context.getEnvironment();
+
+        String[] activeProfiles = env.getActiveProfiles();
+        if (activeProfiles.length > 0) {
+            System.out.println("Active Profile: " + activeProfiles[0]);
+        } else {
+            System.out.println("No active profile, defaulting to 'default'");
+        }
+    }
 
 }
